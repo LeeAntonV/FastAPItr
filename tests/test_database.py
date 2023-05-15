4297a3c74067e7b93590c29358686eea5d1f30d7
LeeAntonV <127417028+LeeAntonV@users.noreply.github.com>
2023-05-15 22:30:16 +0500

    Update test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -10,11 +10,9 @@ from app.database import get_db
 from app.database import Base
 
 
+DATABASE_URL = f'postgresql://{os.environ.get("DATABASE_USERNAME")}:{os.environ.get("DATABASE_PASSWORD")}@{os.environ.get("DATABASE_HOSTNAME")}:{os.environ.get("DATABASE_PORT")}/{os.environ.get("DATABASE_NAME")}'
 
-
-DATABASE_URL = f'postgresql://{os.getenv("DATABASE_USERNAME")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_HOSTNAME")}:{os.getenv("DATABASE_PORT")}/{os.getenv("DATABASE_NAME")}'
-
-
+        
 engine = create_engine(DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(
