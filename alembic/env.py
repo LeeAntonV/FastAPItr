ae1a7e82fcdaf3c3627a24941c65d9818a10bf4c
LeeAntonV <127417028+LeeAntonV@users.noreply.github.com>
2023-05-15 22:38:48 +0500

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,8 @@ from app.config import settings
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option(
-    "sqlalchemy.url", f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
+    "sqlalchemy.url", f'postgresql://{os.environ.get("DATABASE_USERNAME")}:{os.environ.get("DATABASE_PASSWORD")}@{os.environ.get("DATABASE_HOSTNAME")}:{os.environ.get("DATABASE_PORT")}/{os.environ.get("DATABASE_NAME")}'
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
